f127179a7f8c353c10221d65af10d59f9763576d
EliasEduardo07 <134114617+EliasEduardo07@users.noreply.github.com>
2023-05-23 09:56:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import itertools
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def apriori(recetas, min_support):
     ingredientes_frecuentes = {}
@@ -51,15 +53,43 @@ def main():
     ]
 
     # Parámetros
-    min_support = 5
+    min_support = st.slider("Soporte mínimo", min_value=1, max_value=10, value=5)
+    min_confidence = st.slider("Confianza mínima (%)", min_value=1, max_value=100, value=50)
 
     # Ejecutar el algoritmo de Apriori
     conjuntos_frecuentes = apriori(recetas, min_support)
 
-    # Mostrar los conjuntos de ingredientes frecuentes
-    st.subheader("Conjuntos de ingredientes frecuentes:")
+    # Obtener las reglas de asociación
+    reglas_asociacion = []
     for conjunto in conjuntos_frecuentes:
-        st.write(conjunto)
+        if len(conjunto) > 1:
+            for i in range(1, len(conjunto)):
+                for combinacion in itertools.combinations(conjunto, i):
+                    conjunto_A = set(combinacion)
+                    conjunto_B = set(conjunto) - conjunto_A
+                    soporte_A = sum(1 for receta in recetas if conjunto_A.issubset(receta))
+                    soporte_B = sum(1 for receta in recetas if conjunto_B.issubset(receta))
+                    confidence = (soporte_B / soporte_A) * 100
+                    if confidence >= min_confidence:
+                        reglas_asociacion.append((conjunto_A, conjunto_B, soporte_A, soporte_B, confidence))
+
+    # Crear DataFrame con los resultados ordenados por confianza
+    df_resultados = pd.DataFrame(reglas_asociacion, columns=["A", "B", "Soporte A", "Soporte B", "Confianza"])
+    df_resultados = df_resultados.sort_values("Confianza", ascending=False)
+
+    # Mostrar gráfico de barras con las confianzas
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.barh(range(len(df_resultados)), df_resultados["Confianza"], color="skyblue")
+    ax.set_yticks(range(len(df_resultados)))
+    ax.set_yticklabels(["{} -> {}".format(list(row["A"]), list(row["B"])) for _, row in df_resultados.iterrows()])
+    ax.invert_yaxis()
+    ax.set_xlabel("Confianza (%)")
+    ax.set_title("Reglas de Asociación")
+    st.pyplot(fig)
+
+    # Mostrar tabla con los resultados
+    st.subheader("Reglas de Asociación ordenadas por confianza:")
+    st.dataframe(df_resultados)
 
 if __name__ == '__main__':
     main()
